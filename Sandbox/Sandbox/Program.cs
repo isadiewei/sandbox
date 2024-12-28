5d38023cf003c327be4d44ba09e484f481b64ce3
Halt, begrudged <isadiewei@pm.me>
2024-12-28 13:41:19 -0500

    ~ gitignore

---
+++ Sandbox/Sandbox/Program.cs
@@ -0,0 +1,22 @@
+using Sandbox.Database;
+using Sandbox.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
