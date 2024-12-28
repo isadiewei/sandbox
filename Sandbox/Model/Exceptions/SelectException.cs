b687c0d60431b7c3587d7b13f337cd3d11c0b847
Halt, begrudged <isadiewei@pm.me>
2024-12-28 15:18:04 -0500

    + Model/Exceptions(custom examples)

---
+++ Sandbox/Model/Exceptions/SelectException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Exceptions
+{
+    public class SelectException : Exception
+    {
+        public SelectException()
+        {
+        }
+
+        public SelectException(string message)
+            : base(message)
+        {
+        }
+
+        public SelectException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Sandbox/Model/Exceptions/SqlConnectionException.cs
@@ -0,0 +1,9 @@
+﻿namespace Model.Exceptions
+{
+    public class SqlConnectionException : Exception
+    {
+        public SqlConnectionException() { }
+
+        public SqlConnectionException(string message, Exception innerException) : base(message, innerException) { } 
+    }
+}
