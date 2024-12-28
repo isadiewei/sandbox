0645832ec91752836dba68c5fb1d7d719d316792
Halt, begrudged <isadiewei@pm.me>
2024-12-28 15:18:28 -0500

    + Model(csproj)

---
+++ Sandbox/Model/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+    public class Todo
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
