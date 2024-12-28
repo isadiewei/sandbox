b8885218fe0041ba5cfec2e37b75c1cfd0e35632
Halt, begrudged <isadiewei@pm.me>
2024-12-28 15:22:40 -0500

    + TaskController

---
+++ Sandbox/Sandbox/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+using Model;
+
+namespace Sandbox.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly IUserTaskService _userTaskService;
+
+        public TaskController(IUserTaskService userTaskService)
+        {
+            _userTaskService = userTaskService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTasks()
+        {
+            try
+            {
+                IEnumerable<Todo> result = await _userTaskService.GetAll();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
