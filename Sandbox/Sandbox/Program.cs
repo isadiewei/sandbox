1dc983b6cdcc9df4e459af6f430696072b6b97ae
Halt, begrudged <isadiewei@pm.me>
2024-12-28 15:27:48 -0500

    + Program.cs

--- Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Program.cs
@@ -1,13 +1,15 @@
 using Sandbox.Database;
 using Sandbox.Repository;
+using Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddSingleton<DatabaseConnection>();
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<IUserTaskService, UserTaskService>();
 
 var app = builder.Build();
 
